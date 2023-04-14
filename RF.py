9c5912db6021abe13da89f1df18e3b04427dc3da
rameswar-oss <61401412+rameswar-oss@users.noreply.github.com>
2023-04-15 02:56:50 +0530

    Update RF.py

--- RF.py
+++ RF.py
@@ -53,15 +53,7 @@ def diabetes_predd(input_parameters : model_input):
     
     prediction = RF.predict_proba([input_list])
     
-    if (prediction[0][1] >= 0.75):
-        return 2
-    elif (prediction[0][1] < 0.75 and prediction[0][0] >= 0.25):
-
-        return 1
-        
-    else:
-
-        return 0
+    prediction = DT.predict_proba([input_list])
 #python -m uvicorn RF:app --reload
 
 
